5c38010642a2149c161a02a3455c23b7cb946a42
Alexsander <kas-cor@users.noreply.github.com>
2016-06-09 10:53:25 +0300

    Update Yii2ToVestaCP.sh

--- Yii2ToVestaCP.sh
+++ Yii2ToVestaCP.sh
@@ -10,20 +10,23 @@ if [ ! -d "/home/$2/web/$3" ]; then
     exit 1
 fi
 
-cd /home/$2/web/$3
-
-if [[ "$1" ==  "basic" ]]; then
-    composer create-project --prefer-dist yiisoft/yii2-app-basic yii2
+if [[ "$1" == "basic" || "$1" == "advanced" ]]; then
+    cd /home/$2/web/$3
+    composer create-project --prefer-dist yiisoft/yii2-app-$1 yii2
     rm -rf public_html
-    ln -s yii2/web public_html
+    if [[ "$1" == "basic" ]]; then
+        ln -s yii2/web public_html
+    else
+        cd yii2
+        php init
+        cd ..
+        ln -s yii2/frontend/web public_html
+        cd yii2/frontend/web
+        ln -s ../../backend/web admin
+    fi
+    exit 0
 else
-    composer create-project --prefer-dist yiisoft/yii2-app-advanced yii2
-    rm -rf public_html
-    cd yii2
-    php init
-    cd ..
-    ln -s yii2/frontend/web public_html
-    cd yii2/frontend/web
-    ln -s ../../backend/web admin
+    echo "Choese mode: basic or advanced"
+    echo "Ex. Yii2ToVestaCP.sh basic user domen"
+    exit 1
 fi
-exit 0
