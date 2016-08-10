5d88ab06c2a15f6c173de91028ea20fa126bedd3
Alexsander <kas-cor@users.noreply.github.com>
2016-08-10 10:16:16 +0300

    Update Yii2ToVestaCP.sh

--- Yii2ToVestaCP.sh
+++ Yii2ToVestaCP.sh
@@ -2,11 +2,13 @@
 
 if [ ! -d "/home/$2" ]; then
     echo "User $2 not found!"
+    echo "Ex. Yii2ToVestaCP.sh basic user domen"
     exit 1
 fi
 
 if [ ! -d "/home/$2/web/$3" ]; then
     echo "Domen $3 not found!"
+    echo "Ex. Yii2ToVestaCP.sh basic user domen"
     exit 1
 fi
 
